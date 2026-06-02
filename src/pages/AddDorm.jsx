f29874a49878b120e312d2ef6bafbaf43560a8ad
Jazmin Iloso <matminisace15@gmail.com>
2026-06-02 16:40:40 +0800

    feat: accept images in addDorm

--- src/pages/AddDorm.jsx
+++ src/pages/AddDorm.jsx
@@ -9,22 +9,77 @@ export default function AddDorm() {
   const [lng, setLng] = useState(''); 
   const navigate = useNavigate(); 
 
+  const [imageFiles, setImageFiles] = useState(null);
+  const [uploading, setUploading] = useState(false);
+
   const handleAddDorm = async (e) => {
     e.preventDefault(); 
-    const { error } = await supabase.from('dorms').insert(
-      [
-        {
-          name: dormName,
-          price: Number(price),
-          lat: parseFloat(lat),
-          lng: parseFloat(lng)
-        }
-      ]
-    );
 
-    if (error) console.log(`[ERROR] ${error.message}`) 
-    else {
-      navigate('/'); // dashboard
+    try {
+      const { data: newDorm, error: dormError } = await supabase
+        .from('dorms')
+        .insert(
+          [
+            {
+              name: dormName,
+              price: Number(price),
+              lat: parseFloat(lat),
+              lng: parseFloat(lng)
+            }
+          ]
+        )
+        .select() // .select() is require to get back the newly generated Dorm ID 
+        .single();
+      
+      if (dormError) console.log(dormError);
+      console.log(`${dormName} added to table dorms`)
+      
+      const newDormId = newDorm.id; 
+
+      // Loop through the selected image files and upload them 
+      // to the storage
+      setUploading(true);
+      if (imageFiles.length > 0) {
+        const imageRows = []; 
+        // standard for loop to handle async sequential uploads smoothly
+        for (const file of imageFiles){
+          const fileExt = file.name.split('.').pop(); 
+          const fileName = `${Date.now()}-${Math.random().toString(36).substr(2,9)}.${fileExt}`;
+          const filePath = `${fileName}`; 
+
+          // upload current file to the bucket 
+          const { error: uploadError } = await supabase
+            .storage
+            .from('dorm-images')
+            .upload(filePath, file);
+
+          if (uploadError) throw uploadError; 
+
+          // get the public url for the file 
+          const { data } = supabase
+            .storage
+            .from('dorm-images')
+            .getPublicUrl(filePath); 
+          
+          // structure the row data to match our new 'dorm_images' table layout 
+          imageRows.push({
+            dorm_id: newDormId, 
+            image_url: data.publicUrl
+          });
+        }
+
+        // insert all image links into the dorm_images table  
+        const { error: imagesInsertError } = await supabase
+          .from('dorm_images')
+          .insert(imageRows);
+        if (imagesInsertError) throw imagesInsertError;
+      }
+      navigate('/');
+
+    } catch (error) {
+      console.error('[ERROR]', error.message);
+    } finally {
+      setUploading(false);
     }
   }; 
 
@@ -38,7 +93,17 @@ export default function AddDorm() {
         <input type="number" placeholder="Price" value={price} onChange={e => setPrice(e.target.value)} required /><br></br>
         <input type="number" step="any" placeholder="Latitude" value={lat} onChange={e => setLat(e.target.value)} required /><br></br>
         <input type="number" step="any" placeholder="Longitude" value={lng} onChange={e => setLng(e.target.value)} required /><br></br>
-        <button type="submit">Save Listing</button>
+
+        <label>Dorm Image:</label>
+        <input 
+          type="file"
+          accept="image/*"
+          multiple
+          onChange={(e) => setImageFiles(Array.from(e.target.files))} // Convert FileList object to standard JS Array
+        />
+        <button type="submit">
+          {uploading ?  `Uploading ${imageFiles.length} images...`: 'Add Dorm'}
+        </button>
       </form>
     </div>
   )
