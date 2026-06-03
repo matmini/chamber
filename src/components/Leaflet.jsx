8915ecc781f7e8195607c4eeed54c81ceb1c2b9a
Jazmin Iloso <matminisace15@gmail.com>
2026-06-03 18:50:20 +0800

    feat: map listings in the leaflet

--- src/components/Leaflet.jsx
+++ src/components/Leaflet.jsx
@@ -10,11 +10,13 @@ export default function Leaflet( {listings}) {
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
+    {listings.map((listing) => (
+      <Marker key={listing.id}  position={[listing.lat, listing.lng]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    ))}
   </MapContainer>
   )
 }
