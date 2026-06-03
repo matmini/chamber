1b329639919062091fb0efc8a79d2bbd9f33e6de
Jazmin Iloso <matminisace15@gmail.com>
2026-06-03 19:53:01 +0800

    feat [Leaflet]: change default position to demarces area

--- src/components/Leaflet.jsx
+++ src/components/Leaflet.jsx
@@ -1,11 +1,11 @@
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+
 export default function Leaflet( {listings}) {
-  console.log(listings);
-  const position = [14.167405,121.243347]; 
+  const defaultCoordinates = [14.168740,121.244345] // demarces area
 
   return (
-  <MapContainer center={position} zoom={13} scrollWheelZoom={true} style={{height: '250px', width: '400px'}} >
+  <MapContainer center={defaultCoordinates} zoom={14} scrollWheelZoom={true} style={{height: '250px', width: '400px'}} >
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
