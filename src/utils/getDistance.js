47e23958698736c888eafd1d8b88789198a81f05
Jazmin Iloso <matminisace15@gmail.com>
2026-06-03 19:51:18 +0800

    add [utils]: getDistance function

---
+++ src/utils/getDistance.js
@@ -0,0 +1,16 @@
+export async  function getDistance(lat1, lng1, lat2, lng2){
+
+  const url = `https://router.project-osrm.org/route/v1/foot/${lng1},${lat1};${lng2},${lat2}?overview=false`;
+  let distance = null;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json(); 
+    if (data.code === 'Ok') {
+      distance = data.routes[0].distance;
+    }
+  }catch(err) {
+    console.log(`[ERROR] ${err.message}`);
+  }
+  return distance;
+}
