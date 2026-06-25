6f055b0cf0d6996120863d7c20e419ea93f51dce
Jazmin Iloso <matminisace15@gmail.com>
2026-06-25 19:43:55 +0800

    ui: update dormcard

--- src/components/DormCard.jsx
+++ src/components/DormCard.jsx
@@ -1,4 +1,3 @@
-import '../css/dormcard.css';
 import placeholderImage from '../assets/empty.png';
 
 const DormCard = ({ listing , images, onClick}) => {
@@ -8,21 +7,33 @@ const DormCard = ({ listing , images, onClick}) => {
     ? images[0].image_url
     : placeholderImage
   return (
-    <div  className="dorm-card" onClick={onClick}>
-      <img src={displayImage} className="card-picture"/>
-
-      <div className="card-info-overlay">
-        <h4 className="card-name">{listing.name}</h4>
-        <div className="card-meta">
-          <span className="card-price">₱{Number(listing.price).toLocaleString()}/mo</span>
-          {listing.rating? (
-            <span className="card-rating">⭐ {listing.rating.toFixed(1)}</span>
-          ): (
-            <span className="card-rating-new">New</span>
-          )}
-        </div>
+    <div id="card" class="flex flex-col w-48 
+                          group cursor-pointer">
+      <div id="image-container" class="w-full h-48 rounded-sm overflow-hidden">
+        <img src={displayImage} class="w-full h-full object-cover"/>
       </div>
+      <div class="flex flex-col text-start py-2">
+        <h5 id="price" class="group-hover:underline">₱{Number(listing.price).toLocaleString()}/mo</h5>
+        <h6 id="name" class="text-[12px] -my-1 group-hover:underline">{listing.name}</h6>
+        <p class="text-[11px] text-gray-400 group-hover:underline">9706 Banahaw St., Umali Subdivision</p>
+      </div>
+
     </div>
+    // <div  className="dorm-card" onClick={onClick}>
+    //   <img src={displayImage} className="card-picture"/>
+
+    //   <div className="card-info-overlay">
+    //     <h4 className="card-name">{listing.name}</h4>
+    //     <div className="card-meta">
+    //       <span className="card-price">₱{Number(listing.price).toLocaleString()}/mo</span>
+    //       {listing.rating? (
+    //         <span className="card-rating">⭐ {listing.rating.toFixed(1)}</span>
+    //       ): (
+    //         <span className="card-rating-new">New</span>
+    //       )}
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
