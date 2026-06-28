15443ea9e7bd1b88c9626849ac0a259e56b1bb7f
Jazmin Iloso <matminisace15@gmail.com>
2026-06-28 17:49:41 +0800

    feat: add search summary

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -66,35 +66,48 @@ export default function Dashboard() {
   return (
     <div className="flex flex-col items-center ">
       {/* <Navbar></Navbar> */}
-      <div id="search-container" 
+      <div id="filters" 
             className="px-3 py-8 w-full max-w-xl">
-        <input type="text" placeholder="Search dorms or apartments" 
+        <input id="search" type="text" placeholder="Search dorms or apartments" 
                 value={searchQuery}
                 onChange={(e)=> setSearchQuery(e.target.value)}
-                className="w-full px-4 py-2  bg-gray-100 rounded-full focus:outline-none"/>
-      </div>
+                className="w-full px-4 py-2  bg-gray-100 rounded-full focus:outline-none"
+        />
+        <div id="summary-and-sort" className="flex justify-between ">
+          <div id="summary">
+            {searchQuery!=='' && 
+              <div>
+                <h1 className="text-xl font-semibold">{searchQuery}</h1>
+                <p1 className="text-gray-500 text-[12px]">{listings.length} listings found for "{searchQuery}"</p1>
+              </div>
+            }
+          </div>
+          <div id="sort-by" className='flex items-center gap-2 py-3 px-3 self-end'>
+            <p className='text-sm'>Sort By</p>
+            <div className="relative w-48">
+              {/* The Select Dropdown */}
+              <select 
+                value={sortBy} 
+                onChange={(e) => setSortBy(e.target.value)}
+                className="w-full appearance-none rounded-lg border border-gray-300 bg-white px-4 py-2 pr-10 text-sm text-gray-700 shadow-sm  focus:outline-none"
+              >
+                <option value="">Best Match</option>
+                <option value="price-asc">Price low to high</option>
+                <option value="price-desc">Price high to low</option>
+                <option value="distance-asc">Nearest to UP Gate</option>
+              </select>
 
-      <div id="sort-by" className='flex items-center gap-2 py-3 px-3 self-end'>
-        <p className='text-sm'>Sort By</p>
-        <div className="relative w-48">
-          {/* The Select Dropdown */}
-          <select 
-            value={sortBy} 
-            onChange={(e) => setSortBy(e.target.value)}
-            className="w-full appearance-none rounded-lg border border-gray-300 bg-white px-4 py-2 pr-10 text-sm text-gray-700 shadow-sm  focus:outline-none"
-          >
-            <option value="">Best Match</option>
-            <option value="price-asc">Price low to high</option>
-            <option value="price-desc">Price high to low</option>
-            <option value="distance-asc">Nearest to UP Gate</option>
-          </select>
-
-          {/* The Lucide Icon layered on top */}
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-            <ChevronDown className="h-4 w-4 text-gray-500" />
+              {/* The Lucide Icon layered on top */}
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                <ChevronDown className="h-4 w-4 text-gray-500" />
+              </div>
+            </div>
           </div>
         </div>
+        
       </div>
+
+
       
       <DormGallery dorms={listings} images={imageFiles}/> 
 
